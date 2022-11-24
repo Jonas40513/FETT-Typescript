a728c074b3d01bc1d9e6019a6fe1b538da0d7358
honiflo1 <63962003+honiflo1@users.noreply.github.com>
2022-11-24 11:51:37 +0100

    Add Detail Component #5

--- frontend/src/components/app-component.ts
+++ frontend/src/components/app-component.ts
@@ -1,8 +1,9 @@
 import { html, render } from "lit-html"
 import "./overview-component"
-
+import "./detail-component"
 const appComponentTemplate = html`
-    <overview-component id="table"></overview-component>`
+    <overview-component id="table"></overview-component>
+    <detail-component id="detail"></detail-component>`
 
 class AppComponent extends HTMLElement {
     constructor() {
@@ -15,6 +16,14 @@ class AppComponent extends HTMLElement {
     }
     render() {
         render(appComponentTemplate, this.shadowRoot)
+        const overviewComponent = this.shadowRoot.getElementById("table")
+        const detailComponent: HTMLElement = this.shadowRoot.getElementById("detail")
+        overviewComponent.addEventListener("emergency-selected", (e: CustomEvent) => {
+            const emergencyId = e.detail.emergencyId
+            detailComponent.setAttribute("emergency-id", emergencyId)
+            overviewComponent.style.display = "none"
+            detailComponent.style.display = "block"
+        })
     }
 }
 
---
+++ frontend/src/components/detail-component.ts
@@ -0,0 +1,42 @@
+import { html, render } from "lit-html"
+
+import store from "../model/store"
+import { Emergency } from "../model/emergency"
+import { getFormattedDate, getLocation, getDepartments } from "../model/model"
+import { w3css } from "../properties"
+
+const tableTemplate = html`
+    <link rel="stylesheet" href=${w3css}>
+    <table class="w3-table w3-striped w3-bordered">
+        <tbody></tbody>
+    </table>`
+
+const rowTemplate = (emergency: Emergency) => html`
+    <tr><th>Einsatznummer</th><td>${emergency.id}</td></tr>
+    <tr><th>Datum</th><td>${getFormattedDate(emergency.start, emergency.end)}</td></tr>
+    <tr><th>Alarmstufe</th><td>${emergency.level}</td></tr>
+    <tr><th>Ort</th><td>${getLocation(emergency.district, emergency.location, emergency.town)}</td></tr>
+    <tr><th>Feuerwehren</th><td>${getDepartments(emergency.departments)}</td></tr>`
+
+class DetailComponent extends HTMLElement {
+    static get observedAttributes() {
+        return ["emergency-id"]
+    }
+
+    constructor() {
+        super()
+        this.attachShadow({ mode: "open" })
+    }
+
+    attributeChangedCallback(name: string, oldValue: string, value: string) {
+        this.render(store.getValue().emergencies.filter((x) => x.id == value)[0])
+    }
+    private render(emergency: Emergency) {
+        render(tableTemplate, this.shadowRoot)
+        const tBody = this.shadowRoot.querySelector("tbody")
+        const row = tBody.insertRow()
+        render(rowTemplate(emergency), row)
+    }
+}
+
+customElements.define("detail-component", DetailComponent)
